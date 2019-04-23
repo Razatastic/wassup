2020fb4dbb706ddbace891aa2ca5b90940a24112
Ahmad Raza <raza2157@gmail.com>
2019-04-23 03:47:45 -0400

    Created Profile page layout

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,5 +1,27 @@
 import React from "react";
+import { Container, Row } from "reactstrap";
+import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
 
-const Profile = () => <h1>Profile Page</h1>;
+function Profile({ auth, profile }) {
+  if (!auth.uid) return <Redirect to="/login" />;
+  return (
+    <Container>
+      <p className="text-center display-1" style={{ marginTop: 50 }}>
+        {profile.initials}
+      </p>
+      <p className="text-center display-4" style={{ marginTop: 50 }}>
+        Check back soon!
+      </p>
+    </Container>
+  );
+}
 
-export default Profile;
+const mapStateToProps = state => {
+  return {
+    auth: state.firebase.auth,
+    profile: state.firebase.profile
+  };
+};
+
+export default connect(mapStateToProps)(Profile);
