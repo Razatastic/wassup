4f051055c5349868f56a57251841afe7321e37cf
Ahmad Raza <raza2157@gmail.com>
2019-04-22 21:27:17 -0400

    Added compose to combine multiple store enchancers

--- src/index.js
+++ src/index.js
@@ -3,12 +3,24 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./store/reducers/rootReducer";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
+import { reduxFirestore, getFirestore } from "redux-firestore";
+import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
+import fbConfig from "./config/fbConfig";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+// combineReducers allows you to combine multiple reducers into one (rootReducer = combination of other auth and status reducers)
+// Compose allows you to use multiple store enhancers (similar to combineReducers)
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    reduxFirestore(fbConfig),
+    reactReduxFirebase(fbConfig)
+  )
+);
 
 ReactDOM.render(
   <Provider store={store}>
