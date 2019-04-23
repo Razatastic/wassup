5204fef480fe3314b9daae1612011bf13c313985
Ahmad Raza <raza2157@gmail.com>
2019-04-22 21:31:06 -0400

    Added connection to statuses collection from firebase

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -3,6 +3,8 @@ import { Container, Row, Col } from "reactstrap";
 import Notifications from "./Notifications";
 import StatusList from "../status/StatusList";
 import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
 
 const Dashboard = props => {
   const { statuses } = props;
@@ -23,8 +25,12 @@ const Dashboard = props => {
 
 const mapStateToProps = state => {
   return {
-    statuses: state.status.statuses
+    // statuses: state.status.statuses
+    statuses: state.firestore.ordered.statuses
   };
 };
 
-export default connect(mapStateToProps)(Dashboard);
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([{ collection: "statuses" }])
+)(Dashboard);
