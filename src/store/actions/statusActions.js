0a77afe8267180bacdbd5d5d91282f49a61da737
Ahmad Raza <raza2157@gmail.com>
2019-04-22 21:29:38 -0400

    Added async functionality from firebase to statusActions

--- src/store/actions/statusActions.js
+++ src/store/actions/statusActions.js
@@ -1,7 +1,22 @@
 const createStatus = status => {
-  return (dispatch, getState) => {
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
     //make async call to database
-    dispatch({ type: "CREATE_STATUS", status });
+    const firestore = getFirestore();
+    firestore
+      .collection("statuses")
+      .add({
+        ...status,
+        authorFirstName: "john",
+        authorLastName: "doe",
+        authorId: 12345,
+        createdAt: new Date()
+      })
+      .then(() => {
+        dispatch({ type: "CREATE_STATUS", status });
+      })
+      .catch(err => {
+        dispatch({ type: "CREATE_STATUS_ERROR", err });
+      });
   };
 };
 
