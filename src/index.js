f954f44c915976b51c6fa9d9f3b2b50493871436
Ahmad Raza <raza2157@gmail.com>
2019-04-23 18:11:45 -0400

    Converted CSS to SASS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.css";
+import "./styles/main.css";
 import App from "./App";
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./store/reducers/rootReducer";
