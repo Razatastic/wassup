d47d7ffd89415b4965ecdf4a151184bef47be721
Ahmad Raza <raza2157@gmail.com>
2019-04-24 02:08:22 -0400

    Template for Notifications list

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { Container } from "reactstrap";
+import {
+  ListGroup,
+  ListGroupItem,
+  ListGroupItemHeading,
+  ListGroupItemText
+} from "reactstrap";
+import moment from "moment";
 
 const Notifications = () => (
-  <div>
-    <p>Notifications</p>
-  </div>
+  <Container>
+    <h5>Notifications</h5>
+    <ListGroup>
+      <ListGroupItem>
+        <ListGroupItemHeading>Yoshi joined the party</ListGroupItemHeading>
+        <ListGroupItemText>{moment(new Date()).calendar()}</ListGroupItemText>
+      </ListGroupItem>
+    </ListGroup>
+  </Container>
 );
 
 export default Notifications;
