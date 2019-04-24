1e4dff923192d265e7ce288f5665ee69493520a4
Ahmad Raza <raza2157@gmail.com>
2019-04-24 01:02:30 -0400

    Replaced hardcoded status details

--- src/store/actions/statusActions.js
+++ src/store/actions/statusActions.js
@@ -1,14 +1,16 @@
 const createStatus = status => {
   return (dispatch, getState, { getFirebase, getFirestore }) => {
     //make async call to database
+    const profile = getState().firebase.profile;
+    const authorId = getState().firebase.auth.uid;
     const firestore = getFirestore();
     firestore
       .collection("statuses")
       .add({
         ...status,
-        authorFirstName: "john",
-        authorLastName: "doe",
-        authorId: 12345,
+        authorFirstName: profile.firstName,
+        authorLastName: profile.lastName,
+        authorId: authorId,
         createdAt: new Date()
       })
       .then(() => {
